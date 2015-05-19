1dc96999be8d6b4ceea03bd3e32d89bce2d1f4f7
Daniel Mack <daniel@zonque.org>
2015-05-19 15:49:19 +0200

    welcome_controller: order entries by date

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     limit = Time.now - 3.hours
-    @offers = Entry.where(:entry_type => :offer).where("date >= ?", limit)
-    @requests = Entry.where(:entry_type => :request).where("date >= ?", limit)
+    @offers = Entry.where(:entry_type => :offer).where("date >= ?", limit).order(:date)
+    @requests = Entry.where(:entry_type => :request).where("date >= ?", limit).order(:date)
   end
 end
