7413c7448d5796ca4cbd332ebc09775aafbcbc73
Daniel Mack <daniel@zonque.org>
2015-06-19 10:13:59 +0200

    assorted I18n fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
     @entry = entry
 
     I18n.with_locale(@entry.locale) do
-      mail to: @entry.email, subject: "#{Settings.platform_name}: " + t('your_entry_has_been_created', type: @entry.entry_type)
+      mail to: @entry.email, subject: "#{Settings.platform_name}: " + t("your_#{@entry.entry_type}_has_been_created")
     end
   end
 
