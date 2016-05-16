adf6ac93781a101103f18b0a99decb901dbcd5e8
Daniel Mack <daniel@zonque.org>
2016-05-16 17:40:20 +0200

    move from 'key => value' to 'key: value'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     limit = Time.now - 3.hours
-    @offers = Entry.where(:entry_type => :offer).where("date >= ?", limit).order(:date)
-    @requests = Entry.where(:entry_type => :request).where("date >= ?", limit).order(:date)
+    @offers = Entry.where(entry_type: :offer).where("date >= ?", limit).order(:date)
+    @requests = Entry.where(entry_type: :request).where("date >= ?", limit).order(:date)
 
     header = File.read(Rails.root.join("config", "page_header.#{I18n.locale}.md")) rescue nil
     if header
