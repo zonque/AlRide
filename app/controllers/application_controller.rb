f38771755b6ebafc13a2d47fe55b3c4f442fb8f3
Daniel Mack <daniel@zonque.org>
2016-05-17 20:06:38 +0200

    stay compatible with ruby < 2.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
     return params[:locale] if params[:locale].present?
 
     http_locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first rescue nil
-    return http_locale if I18n.available_locales.include? http_locale&.to_sym
+    return http_locale if http_locale and I18n.available_locales.include? http_locale.to_sym
 
     I18n.default_locale
   end
