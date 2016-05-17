5a1c0120b8367848e8aa61fa4a203caa93dcac4d
Daniel Mack <daniel@zonque.org>
2016-05-17 11:28:37 +0200

    ApplicationController: set locale depending on browser preference

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
- 
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = locale_for_request
   end
 
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
+  end
+
+  private
+
+  def locale_for_request
+    return params[:locale] if params[:locale].present?
+
+    http_locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first rescue nil
+    return http_locale if I18n.available_locales.include? http_locale&.to_sym
+
+    I18n.default_locale
   end
 
 end
